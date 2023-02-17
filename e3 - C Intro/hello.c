497c3a62df07eb6b4ca6d0073e01a33f1d586943
anton <antje12@student.sdu.dk>
2023-02-17 11:54:37 +0100

    done

---
+++ e3 - C Intro/hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (int argc, char* argv[])
+{
+  printf("Hello, World\n");
+
+  int i;
+  printf("Enter 1st integer value : ");
+  scanf("%d",&i);
+  printf("You Entered : %d\n",i);
+
+  printf("Enter 2nd integer value : ");
+  scanf("%d",&i);
+  printf("You Entered : %d\n",i);
+
+  return 0;
+}
