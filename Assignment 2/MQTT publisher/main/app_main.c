c624d202947fea76367521a7c70911edc3dce536
anton <antje12@student.sdu.dk>
2023-04-23 18:18:20 +0200

    cleanup

--- Assignment 2/MQTT publisher/main/app_main.c
+++ Assignment 2/MQTT publisher/main/app_main.c
@@ -1,76 +1,61 @@
 #include "freertos/FreeRTOS.h"
-
 #include "nvs_flash.h"
 #include "esp_log.h"
 #include "mqtt_client.h"
-
 #include "driver/adc.h"
 #include "esp_adc_cal.h"
-
 #include "protocol_examples_common.h"
+
+#define CHANNEL CONFIG_ADC_CHANNEL
+#define UNIT CONFIG_ADC_UNIT
+#define FREQUENCY CONFIG_FREQUENCY
+#define SSID CONFIG_WIFI_SSID
+#define PASSWORD CONFIG_WIFI_PASSWORD
+#define BROKER CONFIG_MQTT_BROKER
+#define TOPIC CONFIG_MQTT_TOPIC
+#define DEVICE_ID "Temp_Sensor"
+#define LMT86_GAIN -10.9    // Average sensor gain of the LMT86 temperature sensor
+#define LMT86_OFFSET 2103.0 // Offset voltage of the LMT86 temperature sensor
 
 static const char *TAG = "MQTT_TEMP_PUBLISHER";
 
-#define LMT86_GAIN      -10.9   // Average sensor gain of the LMT86 temperature sensor
-#define LMT86_OFFSET    2103.0  // Offset voltage of the LMT86 temperature sensor
-
-// Replace these
-#define ENDPOINT        "mqtt://192.168.0.152"
-#define DEVICE_ID       "Temp_Sensor"
-
-typedef struct {
-  esp_mqtt_client_handle_t client;
-  uint32_t sample_rate;
-  char topic[32];
-} paramStruct;
-
 bool run;
 static esp_adc_cal_characteristics_t adc1_chars;
-paramStruct params;
 
 uint32_t collectTemp()
 {
     uint32_t sam = adc1_get_raw(ADC1_CHANNEL_5);
-    printf("Raw: %ld \n", sam);
+    //printf("Raw: %ld \n", sam);
     uint32_t voltage = esp_adc_cal_raw_to_voltage(sam, &adc1_chars);
-    printf("Voltage: %ld mV \n", voltage);
+    //printf("Voltage: %ld mV \n", voltage);
     uint32_t temp = (voltage - LMT86_OFFSET) / LMT86_GAIN;
-    printf("Temperature: %ld C \n\n", temp);
+    printf("Temperature: %ld C \n", temp);
     return temp;
 }
 
-void post(paramStruct *params, uint32_t value)
+void post(esp_mqtt_client_handle_t client, uint32_t value)
 {
     char buffer[4];
-	sprintf(buffer, "%ld\n", value);
-    int msg_id = esp_mqtt_client_publish(params->client, params->topic, buffer, 0, 1, 0);
+    sprintf(buffer, "%ld", value);
+    int msg_id = esp_mqtt_client_publish(client, TOPIC, buffer, 0, 1, 0);
     ESP_LOGI(TAG, "sent publish successful: msg_id=%d, msg=%s", msg_id, buffer);
 }
 
 void CollectTemps(void *pvParameters){
-	
-	paramStruct *params = (paramStruct*) pvParameters; 
-	printf("%d", sizeof(uint32_t));
+    esp_mqtt_client_handle_t client = (esp_mqtt_client_handle_t) pvParameters;
+
     esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
     ESP_ERROR_CHECK(adc1_config_width(ADC_WIDTH_BIT_DEFAULT));
     ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_CHANNEL_5, ADC_ATTEN_DB_11));
-    
-	while (run)
-	{
-        uint32_t temp = collectTemp();
-		post(params, temp);
-		vTaskDelay(pdMS_TO_TICKS(params->sample_rate));
-	}
-	
-	vTaskDelete(NULL);
-}
 
-static void start_TempSensor(esp_mqtt_client_handle_t client){
-	params = (paramStruct){ 
-        .client=client,
-        .sample_rate=1000,
-        .topic="topic/temperature"};
-    xTaskCreate(CollectTemps, "CollectTemps", 4096, &params, 1, NULL);        
+    while (run)
+    {
+        uint32_t temp = collectTemp();
+        post(client, temp);
+        vTaskDelay(pdMS_TO_TICKS(FREQUENCY * 1000));
+    }
+
+    vTaskDelete(NULL);
 }
 
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
@@ -79,29 +64,29 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     esp_mqtt_event_handle_t event = event_data;
     esp_mqtt_client_handle_t client = event->client;
 
-    switch ((esp_mqtt_event_id_t)event_id) {
-        case MQTT_EVENT_CONNECTED:
-            ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-            run = true;
-            start_TempSensor(client);
-            break;
-        case MQTT_EVENT_DISCONNECTED:
-            ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-            run = false;
-            break;
-        default:
-            ESP_LOGI(TAG, "Other event id:%d", event->event_id);
-            break;
+    switch ((esp_mqtt_event_id_t)event_id)
+    {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        run = true;
+        xTaskCreate(CollectTemps, "CollectTemps", 4096, client, 1, NULL);
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        run = false;
+        break;
+    default:
+        //ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
     }
 }
 
 static void mqtt_app_start(void)
 {
     esp_mqtt_client_config_t mqtt_cfg = {
-        .broker.address.uri = ENDPOINT,
+        .broker.address.uri = BROKER,
         .credentials.client_id = DEVICE_ID,
-        .credentials.set_null_client_id = false
-    };
+        .credentials.set_null_client_id = false};
 
     esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
