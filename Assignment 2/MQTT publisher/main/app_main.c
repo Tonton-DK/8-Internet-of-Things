ce3d6dbd393e85fa0806d0947c3eb031661a13b4
anton <antje12@student.sdu.dk>
2023-04-23 16:50:33 +0200

    v2

--- Assignment 2/MQTT publisher/main/app_main.c
+++ Assignment 2/MQTT publisher/main/app_main.c
@@ -28,9 +28,8 @@
 #include <string.h>
 #include <time.h>
 
-static const char *TAG = "MQTT_PUBLISHER";
+static const char *TAG = "MQTT_TEMP_PUBLISHER";
 
-#define DEFAULT_VREF    3300.0  // Default ADC reference voltage in mV
 #define LMT86_GAIN      -10.9   // Average sensor gain of the LMT86 temperature sensor
 #define LMT86_OFFSET    2103.0  // Offset voltage of the LMT86 temperature sensor
 
@@ -39,109 +38,59 @@ static const char *TAG = "MQTT_PUBLISHER";
 #define DEVICE_ID       "Temp_Sensor"
 
 bool run;
+static esp_adc_cal_characteristics_t adc1_chars;
 
 typedef struct {
   esp_mqtt_client_handle_t client;
   uint32_t sample_rate;
-  uint32_t batch_size;
-  char topic[12];
-  uint32_t current;
-  uint32_t* batches;
-  time_t* timestamps;
-  char* csv_str;
+  char topic[32];
 } paramStruct;
 
-uint32_t sample(adc1_channel_t channel)
-{
-    return adc1_get_raw(channel);
-}
+paramStruct params;
 
-uint32_t convert(uint32_t raw)
+uint32_t collectTemp()
 {
-    float voltage = (float)raw * DEFAULT_VREF / 4095.0;
-	printf("Voltage: %.2f \n", voltage);
-    return (voltage - LMT86_OFFSET) / LMT86_GAIN;
-}
-
-void insert(uint32_t value, paramStruct *params)
-{
-    params->batches[params->current] = value;
-    params->timestamps[params->current] = time(NULL);
-    params->current++;
-}
-
-void toCSV(paramStruct *params)
-{
-    for (int i = 0; i < params->batch_size; i++) {
-        sprintf(params->csv_str + strlen(params->csv_str), "%llu#%lu,", params->timestamps[i], params->batches[i]);
-    }
-    params->csv_str[strlen(params->csv_str) - 1] = '\0';
-    printf("The CSV string is: %s\n", params->csv_str);
+    uint32_t sam = adc1_get_raw(ADC1_CHANNEL_5);
+    printf("Raw: %ld \n", sam);
+    uint32_t voltage = esp_adc_cal_raw_to_voltage(sam, &adc1_chars);
+    printf("Voltage: %ld mV \n", voltage);
+    uint32_t temp = (voltage - LMT86_OFFSET) / LMT86_GAIN;
+    printf("Temperature: %ld C \n\n", temp);
+    return temp;
 }
 
 void post(paramStruct *params, uint32_t value)
 {
-    char buffer[16];
+    char buffer[4];
 	sprintf(buffer, "%ld\n", value);
-    int msg_id = esp_mqtt_client_publish(params->client, "topic/temperature", buffer, 0, 1, 0);
+    int msg_id = esp_mqtt_client_publish(params->client, params->topic, buffer, 0, 1, 0);
     ESP_LOGI(TAG, "sent publish successful: msg_id=%d, msg=%s", msg_id, buffer);
 }
-
-void reset(paramStruct *params)
-{
-    params->current = 0;
-    memset(params->batches, 0, params->batch_size * sizeof(uint32_t));
-    memset(params->csv_str, 0, params->batch_size * 2 * sizeof(uint32_t));
-}
-
-static esp_adc_cal_characteristics_t adc1_chars;
 
 void CollectTemps(void *pvParameters){
 	
 	paramStruct *params = (paramStruct*) pvParameters; 
-	
+	printf("%d", sizeof(uint32_t));
     esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
     ESP_ERROR_CHECK(adc1_config_width(ADC_WIDTH_BIT_DEFAULT));
     ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_CHANNEL_5, ADC_ATTEN_DB_11));
     
-	reset(params);
 	while (run)
 	{
-        uint32_t sam = adc1_get_raw(ADC1_CHANNEL_5);
-        printf("Raw: %ld \n", sam);
-        uint32_t voltage = esp_adc_cal_raw_to_voltage(sam, &adc1_chars);
-        printf("Voltage: %ld mV \n", voltage);
-        uint32_t temp = (voltage - LMT86_OFFSET) / LMT86_GAIN;
-        printf("Temperature: %ld C \n\n", temp);
-
+        uint32_t temp = collectTemp();
 		post(params, temp);
-		
-        //insert(temp, params);
-		// Send CSV data
-		//if (params->current >= params->batch_size) 
-		//{ 
-		//	toCSV(params);
-		//	reset(params);
-		//}
-		
 		vTaskDelay(pdMS_TO_TICKS(params->sample_rate));
 	}
 	
 	vTaskDelete(NULL);
 }
 
-paramStruct params_TempSensor;
 static void start_TempSensor(esp_mqtt_client_handle_t client){
-	params_TempSensor = (paramStruct){ 
-        .client=client, 
-        .sample_rate=1000, 
-        .batch_size=10, 
-        .topic="Temperature", 
-        .current=0};
-    params_TempSensor.batches = malloc(params_TempSensor.batch_size * sizeof(uint32_t));
-    params_TempSensor.timestamps = malloc(params_TempSensor.batch_size * sizeof(time_t));
-    params_TempSensor.csv_str = malloc(params_TempSensor.batch_size * 2 * sizeof(uint32_t));
-    xTaskCreate(CollectTemps, "CollectTemps_TempSensorCeiling", 4096, &params_TempSensor, 1, NULL);        
+	params = (paramStruct){ 
+        .client=client,
+        .sample_rate=1000,
+        .topic="topic/temperature"};
+    xTaskCreate(CollectTemps, "CollectTemps", 4096, &params, 1, NULL);        
 }
 
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
